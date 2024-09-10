438874ee7965d06fb76cf03f6d37bd1a331441cf
TheHobbistYT <mastergamerk1004@gmail.com>
2024-09-10 07:28:56 -0600

    Added custom tooltips and tags

--- src/main/java/net/hobby/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/hobby/tutorialmod/block/custom/MagicBlock.java
@@ -1,19 +1,25 @@
 package net.hobby.tutorialmod.block.custom;
 
 import net.hobby.tutorialmod.item.ModItems;
+import net.hobby.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
     public MagicBlock(Settings settings) {
@@ -30,11 +36,21 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity ItemEntity) {
-            if(ItemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if(isValidItem(ItemEntity.getStack())) {
                 ItemEntity.setStack(new ItemStack(Items.DIAMOND, ItemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.magic_block.tooltip"));
+        super.appendTooltip(stack, context, tooltip, options);
     }
 }
