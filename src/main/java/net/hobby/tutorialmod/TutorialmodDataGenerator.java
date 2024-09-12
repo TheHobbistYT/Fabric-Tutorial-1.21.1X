685aea48858ffedc3d22cb025e1a05a0b5832573
TheHobbistYT <mastergamerk1004@gmail.com>
2024-09-12 13:09:52 -0600

    Changed to Datagen

--- src/main/java/net/hobby/tutorialmod/TutorialmodDataGenerator.java
+++ src/main/java/net/hobby/tutorialmod/TutorialmodDataGenerator.java
@@ -2,10 +2,17 @@ package net.hobby.tutorialmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.hobby.tutorialmod.datagen.*;
 
 public class TutorialmodDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
